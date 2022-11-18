2274e5a05ec1f7b25bbbed324f8bbea86cd23c34
Keno9184 <amanogouyaman@gmail.com>
2022-11-18 17:24:45 +0900

    手直しいたした！

--- main/main.py
+++ main/main.py
@@ -3,6 +3,10 @@ import argparse
 from pathlib import Path
 
 parent_dir = str(Path(__file__).parent.parent)
+sys.path.append(parent_dir + r'\console')
+from console import Console
+
+parent_dir = str(Path(__file__).parent.parent)
 sys.path.append(parent_dir + r'\andr4t')
 from andr4t import Andr4t
 
@@ -23,23 +27,23 @@ def __args_analyze_ip(args):
     IP_ADDRESS_MAX = 255
 
     if(not args.arg1 is ARG_IP_ADDRESS_PREFIX):
-        print("-aを先頭字として入力してください")
+        console.print('Please enter "-a" as the first character.')
         return False
 
     iplist=args.arg2.split('.')
 
     if(not len(iplist) is IP_ADDRESS_OCTET_MAX):
-        print("IPアドレスの記述にミスがあります。")
+        Console.print("There is a mistake in the description of the IP address.")
         return False
 
     for ip in iplist:
         try:
             if(IP_ADDRESS_MIN > int(ip) or
                 IP_ADDRESS_MAX < int(ip)):
-                print("エラー")
+                Console.print("error")
                 return False
         except Exception as e:
-            print(e)
+            Console.print(e)
             return False
     return True
 
@@ -48,13 +52,13 @@ def __args_analyze_port(args):
     ARG_port_ADDRESS_PREFIX = "-p"
 
     if(not args.arg3 is ARG_port_ADDRESS_PREFIX):
-        print("-pを先頭字として入力してください")
+        Console.print('Please enter "-p" as the first character.')
         return False
     
     try:
         int(args.arg4)
     except:
-        print("port番号は数値のみ入力してください")
+        Console.print("Please enter only numerical values for the port number")
         return False
     
     return True
@@ -66,26 +70,22 @@ def main() -> None:
     """
     #args = sys.argv   #sysによる引数の取得。argsにリストで格納
     # TODO args parserを用いて、socket通信用のip, portを指定する。
-    parser = argparse.ArgumentParser(descriotion="コマンドラインの引数を取得")   #パーサー作成
+    parser = argparse.ArgumentParser(description="Get command line arguments")   #パーサー作成
     parser.add_argument("arg1",help="-a",type=str)   #引数設定
-    parser.add_argument("arg2",help="IPアドレス",type=str)
+    parser.add_argument("arg2",help="IP address",type=str)
     parser.add_argument("arg3",help="-p",type=str)
-    parser.add_argument("arg4",help="TCP port番号",type=str)
+    parser.add_argument("arg4",help="TCP port number",type=str)
     args = parser.parse_args( )   #引数解析
 
-    __args_analyze_ip(args)
-    __args_analyze_port(args)
-
-    if(not(__args_analyze_ip(args) and __args_analyze_port(args))):
+    if(not __args_analyze_ip(args) or
+        not __args_analyze_port(args)):
         return False
 
-    IP=args.arg2
+    ip=args.arg2
     port=int(args.arg4)
     
-    android = Andr4t(IP,port) #androidはAndr4tのインスタンス
+    android = Andr4t(ip,port) #androidはAndr4tのインスタンス
     android.get_shell()
-
-
 
 if (__name__ == "__main__"):
     main()
