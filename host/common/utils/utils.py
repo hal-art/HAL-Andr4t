b7ac5b4a833bb6597fc88da3e7e0ac83e1a1150e
Hal <halcorder@gmail.com>
2022-11-20 12:46:10 +0900

    関数ヘッダ・クラスヘッダを追加

--- host/common/utils/utils.py
+++ host/common/utils/utils.py
@@ -1,5 +1,21 @@
 class Utils:
+    """
+    Utilsクラス
+    
+    細かい共通処理などはここに記載する。
+    
+    """
     def int_try_parse(value: str) -> tuple(bool, int):
+        """
+        str -> intへの変換処理
+
+        Args:
+            value (str): 変換したい文字列
+
+        Returns:
+            bool: 変換できるかどうか
+            int: 変換後の値
+        """
         try:
             int_value = int(str)
             return (True, int_value)
